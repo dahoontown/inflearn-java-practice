24112657f5780613bcc86249f1dc2227da1691e3
HUN <HUN@DESKTOP-UU6CIL8>
2020-07-06 19:11:33 +0900

    Init calender

---
+++ src/hun/celender/celender.java
@@ -0,0 +1,14 @@
+package hun.celender;
+
+public class celender {
+
+	public celender() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world!");
+	}
+
+}
